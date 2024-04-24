bee8503a671ebb2c5a8f2edad2163e2fdc1bb42b
alimov <alikcye.2001@gmail.com>
2024-04-24 21:29:20 +0300

    sdfsdf

--- src/main/java/com/example/videoChat/handler/SocketHandler.java
+++ src/main/java/com/example/videoChat/handler/SocketHandler.java
@@ -1,42 +1,69 @@
 package com.example.videoChat.handler;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import jakarta.websocket.*;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class SocketHandler extends TextWebSocketHandler{
-    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+    private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
 
-    @Override
-    public void handleTextMessage(WebSocketSession session,TextMessage textMessage){
-        System.out.println("Received a text message"+textMessage.getPayload());
-        try{
-            for(WebSocketSession webSocketSession:sessions){
-                if(webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())){
-                    webSocketSession.sendMessage(textMessage);
-                }
-            }
-        }
-        catch(Exception e){
-            throw new RuntimeException();
-        }
-    }
-
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session){
+    @OnOpen
+    public void whenOpening(Session session) throws IOException, EncodeException {
+        System.out.println("Open!");
+        // Add websocket session to a global set to use in OnMessage.
         sessions.add(session);
     }
 
+    @OnMessage
+    public void process(String data, Session session) throws IOException {
+        System.out.println("Got signal - " + data);
+        /*
+         * When signal is received, send it to other participants other than self. In
+         * real world, signal should be sent to only participant's who belong to
+         * particular video conference.
+         */
+        for (Session sess : sessions) {
+            if (!sess.equals(session)) {
+                sess.getBasicRemote().sendText(data);
+            }
+        }
+    }
+
+    @OnClose
+    public void whenClosing(Session session) {
+        System.out.println("Close!");
+        sessions.remove(session);
+    }
+//    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+//
 //    @Override
-//    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-//        System.out.println(session.getId());
-//        System.out.println(message.getPayload());
-//        super.handleMessage(session, message);
+//    public void handleTextMessage(WebSocketSession session,TextMessage textMessage){
+//        System.out.println("Received a text message"+textMessage.getPayload());
+//        try{
+//            for(WebSocketSession webSocketSession:sessions){
+//                if(webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())){
+//                    webSocketSession.sendMessage(textMessage);
+//                }
+//            }
+//        }
+//        catch(Exception e){
+//            throw new RuntimeException();
+//        }
+//    }
+//
+//
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session){
+//        sessions.add(session);
 //    }
 
 }
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,212 +1,221 @@
-'use strict';
-
-const startButton = document.getElementById('startButton');
-const callButton = document.getElementById('callButton');
-const hangupButton = document.getElementById('hangupButton');
-callButton.disabled = true;
-hangupButton.disabled = true;
-startButton.addEventListener('click', start);
-callButton.addEventListener('click', call);
-hangupButton.addEventListener('click', hangup);
-
-let startTime;
-const localVideo = document.getElementById('localVideo');
-const remoteVideo = document.getElementById('remoteVideo');
-
-localVideo.addEventListener('loadedmetadata', function() {
-  console.log(`Local video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
-});
-
-remoteVideo.addEventListener('loadedmetadata', function() {
-  console.log(`Remote video videoWidth: ${this.videoWidth}px,  videoHeight: ${this.videoHeight}px`);
-});
-
-remoteVideo.addEventListener('resize', () => {
-  console.log(`Remote video size changed to ${remoteVideo.videoWidth}x${remoteVideo.videoHeight}`);
-  // We'll use the first onsize callback as an indication that video has started
-  // playing out.
-  if (startTime) {
-    const elapsedTime = window.performance.now() - startTime;
-    console.log('Setup time: ' + elapsedTime.toFixed(3) + 'ms');
-    startTime = null;
-  }
-});
-
-let localStream;
-let pc1;
-let pc2;
-const offerOptions = {
-  offerToReceiveAudio: 1,
-  offerToReceiveVideo: 1
+var peerConnection;
+ 
+/*
+ * Setup 'leaveButton' button function.
+ */
+const leaveButton = document.getElementById('leaveButton');
+leaveButton.addEventListener('click', leave);
+ 
+function leave() {
+    console.log('Ending call');
+    peerConnection.close();
+    signalingWebsocket.close();
+    window.location.href = './index.html';
 };
-
-function getName(pc) {
-  return (pc === pc1) ? 'pc1' : 'pc2';
-}
-
-function getOtherPc(pc) {
-  return (pc === pc1) ? pc2 : pc1;
-}
-
-async function start() {
-  console.log('Requesting local stream');
-  startButton.disabled = true;
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
-    console.log('Received local stream');
-    localVideo.srcObject = stream;
-    localStream = stream;
-    callButton.disabled = false;
-  } catch (e) {
-    alert(`getUserMedia() error: ${e.name}`);
-  }
-}
-
-function getSelectedSdpSemantics() {
-  const sdpSemanticsSelect = document.querySelector('#sdpSemantics');
-  const option = sdpSemanticsSelect.options[sdpSemanticsSelect.selectedIndex];
-  return option.value === '' ? {} : {sdpSemantics: option.value};
-}
-
-async function call() {
-  callButton.disabled = true;
-  hangupButton.disabled = false;
-  console.log('Starting call');
-  startTime = window.performance.now();
-  const videoTracks = localStream.getVideoTracks();
-  const audioTracks = localStream.getAudioTracks();
-  if (videoTracks.length > 0) {
-    console.log(`Using video device: ${videoTracks[0].label}`);
-  }
-  if (audioTracks.length > 0) {
-    console.log(`Using audio device: ${audioTracks[0].label}`);
-  }
-  const configuration = getSelectedSdpSemantics();
-  console.log('RTCPeerConnection configuration:', configuration);
-  pc1 = new RTCPeerConnection(configuration);
-  console.log('Created local peer connection object pc1');
-  pc1.addEventListener('icecandidate', e => onIceCandidate(pc1, e));
-  pc2 = new RTCPeerConnection(configuration);
-  console.log('Created remote peer connection object pc2');
-  pc2.addEventListener('icecandidate', e => onIceCandidate(pc2, e));
-  pc1.addEventListener('iceconnectionstatechange', e => onIceStateChange(pc1, e));
-  pc2.addEventListener('iceconnectionstatechange', e => onIceStateChange(pc2, e));
-  pc2.addEventListener('track', gotRemoteStream);
-
-  localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
-  console.log('Added local stream to pc1');
-
-  try {
-    console.log('pc1 createOffer start');
-    const offer = await pc1.createOffer(offerOptions);
-    await onCreateOfferSuccess(offer);
-  } catch (e) {
-    onCreateSessionDescriptionError(e);
-  }
-}
-
-function onCreateSessionDescriptionError(error) {
-  console.log(`Failed to create session description: ${error.toString()}`);
-}
-
-async function onCreateOfferSuccess(desc) {
-  console.log(`Offer from pc1\n${desc.sdp}`);
-  console.log('pc1 setLocalDescription start');
-  try {
-    await pc1.setLocalDescription(desc);
-    onSetLocalSuccess(pc1);
-  } catch (e) {
-    onSetSessionDescriptionError();
-  }
-
-  console.log('pc2 setRemoteDescription start');
-  try {
-    await pc2.setRemoteDescription(desc);
-    onSetRemoteSuccess(pc2);
-  } catch (e) {
-    onSetSessionDescriptionError();
-  }
-
-  console.log('pc2 createAnswer start');
-  // Since the 'remote' side has no media stream we need
-  // to pass in the right constraints in order for it to
-  // accept the incoming offer of audio and video.
-  try {
-    const answer = await pc2.createAnswer();
-    await onCreateAnswerSuccess(answer);
-  } catch (e) {
-    onCreateSessionDescriptionError(e);
-  }
-}
-
-function onSetLocalSuccess(pc) {
-  console.log(`${getName(pc)} setLocalDescription complete`);
-}
-
-function onSetRemoteSuccess(pc) {
-  console.log(`${getName(pc)} setRemoteDescription complete`);
-}
-
-function onSetSessionDescriptionError(error) {
-  console.log(`Failed to set session description: ${error.toString()}`);
-}
-
-function gotRemoteStream(e) {
-  if (remoteVideo.srcObject !== e.streams[0]) {
-    remoteVideo.srcObject = e.streams[0];
-    console.log('pc2 received remote stream');
-  }
-}
-
-async function onCreateAnswerSuccess(desc) {
-  console.log(`Answer from pc2:\n${desc.sdp}`);
-  console.log('pc2 setLocalDescription start');
-  try {
-    await pc2.setLocalDescription(desc);
-    onSetLocalSuccess(pc2);
-  } catch (e) {
-    onSetSessionDescriptionError(e);
-  }
-  console.log('pc1 setRemoteDescription start');
-  try {
-    await pc1.setRemoteDescription(desc);
-    onSetRemoteSuccess(pc1);
-  } catch (e) {
-    onSetSessionDescriptionError(e);
-  }
-}
-
-async function onIceCandidate(pc, event) {
-  try {
-    await (getOtherPc(pc).addIceCandidate(event.candidate));
-    onAddIceCandidateSuccess(pc);
-  } catch (e) {
-    onAddIceCandidateError(pc, e);
-  }
-  console.log(`${getName(pc)} ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
-}
-
-function onAddIceCandidateSuccess(pc) {
-  console.log(`${getName(pc)} addIceCandidate success`);
-}
-
-function onAddIceCandidateError(pc, error) {
-  console.log(`${getName(pc)} failed to add ICE Candidate: ${error.toString()}`);
-}
-
-function onIceStateChange(pc, event) {
-  if (pc) {
-    console.log(`${getName(pc)} ICE state: ${pc.iceConnectionState}`);
-    console.log('ICE state change event: ', event);
-  }
-}
-
-function hangup() {
-  console.log('Ending call');
-  pc1.close();
-  pc2.close();
-  pc1 = null;
-  pc2 = null;
-  hangupButton.disabled = true;
-  callButton.disabled = false;
-}
+ 
+/*
+ * Prepare websocket for signaling server endpoint.
+ */
+var signalingWebsocket = new WebSocket("ws://" + window.location.host +
+    "/video-conf-tutorial/signal");
+ 
+signalingWebsocket.onmessage = function(msg) {
+    console.log("Got message", msg.data);
+    var signal = JSON.parse(msg.data);
+    switch (signal.type) {
+        case "offer":
+            handleOffer(signal);
+            break;
+        case "answer":
+            handleAnswer(signal);
+            break;
+        // In local network, ICE candidates might not be generated.
+        case "candidate":
+            handleCandidate(signal);
+            break;
+        default:
+            break;
+    }
+};
+ 
+signalingWebsocket.onopen = init();
+ 
+function sendSignal(signal) {
+    if (signalingWebsocket.readyState == 1) {
+        signalingWebsocket.send(JSON.stringify(signal));
+    }
+};
+ 
+/*
+ * Initialize
+ */
+function init() {
+    console.log("Connected to signaling endpoint. Now initializing.");    
+    preparePeerConnection();
+    displayLocalStreamAndSignal(true);
+ 
+};
+ 
+/*
+ * Prepare RTCPeerConnection & setup event handlers.
+ */
+function preparePeerConnection() {
+    
+     // Using free public google STUN server.
+    const configuration = {
+        iceServers: [{
+            urls: 'stun:stun.l.google.com:19302'
+        }]
+    };
+ 
+    // Prepare peer connection object
+    peerConnection = new RTCPeerConnection(configuration);
+    peerConnection.onnegotiationneeded = async () => {
+        console.log('onnegotiationneeded');
+        sendOfferSignal();
+    };
+    peerConnection.onicecandidate = function(event) {
+        if (event.candidate) {
+        	sendSignal(event);
+        }
+    };
+    
+    /*
+	 * Track other participant's remote stream & display in UI when available.
+	 * 
+	 * This is how other participant's video & audio will start showing up on my
+	 * browser as soon as his local stream added to track of peer connection in
+	 * his UI.
+	 */
+    peerConnection.addEventListener('track', displayRemoteStream);
+ 
+};
+ 
+/*
+ * Display my local webcam & audio on UI.
+ */
+async function displayLocalStreamAndSignal(firstTime) {
+    console.log('Requesting local stream');
+    const localVideo = document.getElementById('localVideo');
+    let localStream;
+    try {
+        // Capture local video & audio stream & set to local <video> DOM
+        // element
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        });
+        console.log('Received local stream');
+        localVideo.srcObject = stream;
+        localStream = stream;
+        logVideoAudioTrackInfo(localStream);
+ 
+        // For first time, add local stream to peer connection.
+        if (firstTime) {
+            setTimeout(
+                function() {
+                    addLocalStreamToPeerConnection(localStream);
+                }, 2000);
+        }
+ 
+        // Send offer signal to signaling server endpoint.
+        sendOfferSignal();
+ 
+    } catch (e) {
+        alert(`getUserMedia() error: ${e.name}`);
+        throw e;
+    }
+    console.log('Start complete');
+};
+ 
+/*
+ * Add local webcam & audio stream to peer connection so that other
+ * participant's UI will be notified using 'track' event.
+ * 
+ * This is how my video & audio is sent to other participant's UI.
+ */
+async function addLocalStreamToPeerConnection(localStream) {
+    console.log('Starting addLocalStreamToPeerConnection');
+    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+    console.log('localStream tracks added');
+};
+ 
+/*
+ * Display remote webcam & audio in UI.
+ */
+function displayRemoteStream(e) {
+    console.log('displayRemoteStream');
+    const remoteVideo = document.getElementById('remoteVideo');
+    if (remoteVideo.srcObject !== e.streams[0]) {
+        remoteVideo.srcObject = e.streams[0];
+        console.log('pc2 received remote stream');
+    }
+};
+ 
+/*
+ * Send offer to signaling server. This is kind of telling server that my webcam &
+ * audio is ready, so notify other participant of my information so that he can
+ * view & hear me using 'track' event.
+ */
+function sendOfferSignal() {
+    peerConnection.createOffer(function(offer) {
+        sendSignal(offer);
+        peerConnection.setLocalDescription(offer);
+    }, function(error) {
+        alert("Error creating an offer");
+    });
+};
+ 
+/*
+ * Handle the offer sent by other participant & send back answer to complete the
+ * handshake.
+ */
+function handleOffer(offer) {
+    peerConnection
+        .setRemoteDescription(new RTCSessionDescription(offer));
+ 
+    // create and send an answer to an offer
+    peerConnection.createAnswer(function(answer) {
+        peerConnection.setLocalDescription(answer);
+        sendSignal(answer);
+    }, function(error) {
+        alert("Error creating an answer");
+    });
+ 
+};
+ 
+/*
+ * Finish the handshake by receiving the answer. Now Peer-to-peer connection is
+ * established between my browser & other participant's browser. Since both
+ * participants are tracking each others stream, they both will be able to view &
+ * hear each other.
+ */
+function handleAnswer(answer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(
+        answer));
+    console.log("connection established successfully!!");
+};
+ 
+/*
+ * Add received ICE candidate to connection. ICE candidate has information about
+ * how to connect to remote participant's browser. In local LAN connection, ICE
+ * candidate might not be generated.
+ */
+function handleCandidate(candidate) {
+	alert("handleCandidate");
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+};
+ 
+/*
+ * Logs names of your webcam & microphone to console just for FYI.
+ */
+function logVideoAudioTrackInfo(localStream) {
+    const videoTracks = localStream.getVideoTracks();
+    const audioTracks = localStream.getAudioTracks();
+    if (videoTracks.length > 0) {
+        console.log(`Using video device: ${videoTracks[0].label}`);
+    }
+    if (audioTracks.length > 0) {
+        console.log(`Using audio device: ${audioTracks[0].label}`);
+    }
+};
